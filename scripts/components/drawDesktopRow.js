578fc07506f7c9239f8084cc7ebcb67060270bd0
Jørgen Jacobsen <jorgeja@gmail.com>
2023-01-09 17:54:39 +0100

    desktop rows

--- scripts/components/drawDesktopRow.js
+++ scripts/components/drawDesktopRow.js
@@ -5,24 +5,18 @@
  * @param {*} selector
  */
 export const initDesktopRowLoading = (numberOfElements = 0) => {
-  let element = document.createElement('div');
+  let element = document.createElement("div");
   element.innerHTML = `<div class="row desktop-rowadded">`;
   return element;
 };
-export const initDesktopRow = (
-  desktopRowNode,
-  articlesSectionsData
-) => {
+export const initDesktopRow = (desktopRowNode, articlesSectionsData) => {
   desktopRowNode.innerHTML = articlesSectionsData
     .map((articleData) => drawDesktopRow(articleData))
-    .join('');
+    .join("");
 };
 
 function drawDesktopRow(articlesData) {
-  console.log('articlesData', articlesData);
-  let articlesGrid = 'multiple';
-  if (articlesData.articles.length < 2) articlesGrid = 'single';
-  if (articlesData.articles.length == 2) articlesGrid = 'dual';
+  console.log("articlesData", articlesData);
   return `
     <div class="row desktop-row ${articlesData.style}">
       <div class="heading">
@@ -30,111 +24,184 @@ function drawDesktopRow(articlesData) {
         ${
           articlesData.description
             ? `<p class="heading-description">${articlesData.description}</p>`
-            : ''
+            : ""
         }
       </div>
-      <div class="${articlesGrid} ${articlesData.layout}">
+      <div class="${articlesData.layout ? articlesData.layout : "multiple"}">
       ${articlesData.articles
-        .map(
-          (article, index) => `
-        <article
-              id="article_${article.id}"
-              class="preview columns large-12 small-12 medium-12 compact"
-              itemscope
-              itemprop="itemListElement"
-              itemtype="https://schema.org/ListItem"
-              role="article"
-              data-id="${article.id}"
-              data-label=""
-            >
-              <a
-                itemprop="url"
-                href="${article.published_url}"
-              >
-                <figure class="">
-                  <img
-                    class=""
-                    itemprop="image"
-                    alt="image: ${article.title}"
-                    src="${`https://www.kode24.no/images/${article.image}.jpg?imageId=${article.image}&width=1000&compression=80`}"
-                  />
-                </figure>
-
-                <div class="article-preview-text">
-                  <h1 class="headline">
-                    <span class="headline-title-wrapper">
-                      ${article.title}
-                    </span>
-                  </h1>
-
-                  <p class="standfirst">
-                    ${article.subtitle}
-                  </p>
-
-                  <time
-                    itemprop="datePublished"
-                    data-from-now
-                    class="published"
-                    datetime="${article.published}"
-                    title="${article.published}"
-                    >${article.published}</time
-                  >
-                </div>
-              </a>
-              <div class="article-social">
-                <div class="byline-row">
-                <div class="byline-profile-image">
-                  <img src="https://www.kode24.no/images${
-                    article.byline.imageUrl
-                  }" loading="lazy">
-                </div>
-                <div class="byline-info">
-                  <div class="byline-name">${
-                    article.byline.name
-                  }</div>
-                  <div class="byline-bio">${article.byline.bio}</div>
-                </div>
-            </div>
-
-            <div class="social-buttons">
-                <div class="article-social-reactions article-social-item">
-                    <a href="https://www.kode24.no/${
-                      article.id
-                    }#hyvor-talk-view" class="reaction-button reaction">${
-            article.reactions.reactions_count
-              ? article.reactions.reactions_count
-              : ''
-          }</a>
-                </div>
-                <div class="article-social-comments article-social-item">
-                    <a href="https://www.kode24.no/${
-                      article.id
-                    }#hyvor-talk-view" class="reaction-button comment">${
-            article.reactions.comments_count
-              ? article.reactions.comments_count
-              : ''
-          }</a>
-                </div>
-            </div>
-            <div class="article-social-tags article-social-item">
-                  ${article.tags
-                    .split(', ')
-                    .filter((tag) => tag !== 'artikkel')
-                    .slice(0, 2)
-                    .map(
-                      (tag) =>
-                        `<a class="social-tag" href="https://www.kode24.no/emne/${tag}">#${tag}</a>`
-                    )}
-            </div>
-          </div>
-        </article>
-
-      `
+        .map((article, index) =>
+          articlesData.style === "commercial"
+            ? drawDesktopAd(article)
+            : drawDesktopArticle(article)
         )
-        .join('')}
+        .join("")}
       </div>
     </div>
   `;
-
-  return row;
 }
+
+const drawDesktopArticle = (article) => {
+  console.log("article", article);
+  return `
+    <article
+    id="article_${article.id}"
+    class="preview columns large-12 small-12 medium-12 compact"
+    itemscope
+    itemprop="itemListElement"
+    itemtype="https://schema.org/ListItem"
+    role="article"
+    data-id="${article.id}"
+    data-label=""
+    >
+      <a
+        itemprop="url"
+        href="${article.published_url}"
+      >
+        <figure class="">
+          <img
+            class=""
+            itemprop="image"
+            alt="image: ${article.title}"
+            src="${`https://www.kode24.no/images/${article.image}.jpg?imageId=${article.image}&width=1000&compression=80`}"
+          />
+        </figure>
+
+        <div class="article-preview-text">
+
+          <h1 class="headline">
+            <span class="headline-title-wrapper">
+              ${article.title}
+            </span>
+          </h1>
+
+          <p class="standfirst">
+            ${article.subtitle}
+          </p>
+
+          <time
+            itemprop="datePublished"
+            data-from-now
+            class="published"
+            datetime="${article.published}"
+            title="${article.published}"
+          >
+            ${article.published}
+          </time>
+        </div>
+      </a>
+      <div class="article-social">
+        <div class="byline-row">
+          <div class="byline-profile-image">
+            <img src="https://www.kode24.no/images${
+              article.byline.imageUrl
+            }" loading="lazy">
+          </div>
+          <div class="byline-info">
+            <div class="byline-name">${article.byline.name}</div>
+            <div class="byline-bio">${article.byline.bio}</div>
+          </div>
+        </div>
+        <div class="social-buttons">
+          <div class="article-social-reactions article-social-item">
+            <a href="https://www.kode24.no/${
+              article.id
+            }#hyvor-talk-view" class="reaction-button reaction">
+              ${
+                article.reactions.reactions_count
+                  ? article.reactions.reactions_count
+                  : ""
+              }
+            </a>
+          </div>
+          <div class="article-social-comments article-social-item">
+            <a href="https://www.kode24.no/${
+              article.id
+            }#hyvor-talk-view" class="reaction-button comment">
+              ${
+                article.reactions.comments_count
+                  ? article.reactions.comments_count
+                  : ""
+              }
+            </a>
+          </div>
+          <div class="article-social-tags article-social-item">
+            ${article.tags
+              .split(", ")
+              .filter((tag) => tag !== "artikkel")
+              .slice(0, 2)
+              .map(
+                (tag) =>
+                  `<a class="social-tag" href="https://www.kode24.no/emne/${tag}">#${tag}</a>`
+              )}
+          </div>
+        </div>
+      </div>  
+    </article>
+  `;
+};
+
+const drawDesktopAd = (content) => {
+  console.log("content", content);
+  return `
+    <article
+    id="article_${content.id}"
+    class="preview columns large-12 small-12 medium-12 compact commercial-content"
+    itemscope
+    itemprop="itemListElement"
+    itemtype="https://schema.org/ListItem"
+    role="content"
+    data-id="${content.id}"
+    data-label=""
+    >
+      <a
+        itemprop="url"
+        href="${content.published_url}"
+      >
+        <figure class="">
+          <img
+            class=""
+            itemprop="image"
+            alt="image: ${content.title}"
+            src="${`https://www.kode24.no/images/${content.image}.jpg?imageId=${content.image}&width=1000&compression=80`}"
+          />
+        </figure>
+
+        <div class="article-preview-text">
+          <p class="company-name">
+            Annonsørinnhold
+          </p>
+          <h1 class="headline">
+            <span class="headline-title-wrapper">
+              ${content.title}
+            </span>
+          </h1>
+
+
+
+          <time
+            itemprop="datePublished"
+            data-from-now
+            class="published"
+            datetime="${content.published}"
+            title="${content.published}"
+          >
+            ${content.published}
+          </time>
+        </div>
+      </a>
+      <div class="article-social">
+        <div class="byline-row">
+          <div class="byline-profile-image">
+            <img src="https://www.kode24.no/images${
+              content.company.imageUrl
+            }" loading="lazy">
+          </div>
+          <div class="byline-info">
+            <div class="byline-name">${content.company.name}</div>
+            <div class="byline-bio">${content.company.bio}</div>
+          </div>
+        </div>
+      </div>  
+    </article>
+  `;
+};
--- scripts/front.js
+++ scripts/front.js
@@ -1,25 +1,12 @@
-import { initCommon } from './common';
-import { initFrontAdComponents } from './components/frontAdComponents';
-import { getArticlePreviewList } from './functions/getArticlePreviewList';
-import { getFrontPreviewData } from './API/api';
-import { drawCommentRow } from './components/drawCommentRow';
-import { getNodes } from './functions/getNodes';
-import { getPrettyFormatDateString } from './functions/getPrettyFormatDateString';
-import { trackInScreenImpressions } from './functions/inScreenAdImpressionTracker';
-import { shuffleArray } from './functions/shuffleArray';
-import { limitArray } from './functions/limitArray';
-import {
-  initDesktopRowLoading,
-  initDesktopRow,
-} from './components/drawDesktopRow';
-import {
-  initJobAdsComponent,
-  initJobAdsComponentLoading,
-} from './components/JobAdsComponent';
-import {
-  initEventCardsListLoading,
-  initEventCardsList,
-} from './components/eventCardsList';
+import { initCommon } from "./common";
+import { initFrontAdComponents } from "./components/frontAdComponents";
+import { getArticlePreviewList } from "./functions/getArticlePreviewList";
+import { getFrontPreviewData } from "./API/api";
+import { drawCommentRow } from "./components/drawCommentRow";
+import { getNodes } from "./functions/getNodes";
+import { trackInScreenImpressions } from "./functions/inScreenAdImpressionTracker";
+import { shuffleArray } from "./functions/shuffleArray";
+import { limitArray } from "./functions/limitArray";
 
 /**
  * inits everything that needs to run on a kode24 front (/, /emne/react, etc...)
@@ -28,39 +15,13 @@ export async function initFrontend() {
   // get ids and nodes of all article previews on page
   const { articleIds, articlesList } = getArticlePreviewList();
   // get data for all article previews on page
-  const articlePreviewListData = await getFrontPreviewData(
-    articleIds
-  );
+  const articlePreviewListData = await getFrontPreviewData(articleIds);
   // draw bylines for all article previews on page
   //drawByline(articlePreviewListData, articleIds, articlesList);
   // draw comment rows for all article previews on page
   drawCommentRow(articlePreviewListData, articleIds, articlesList);
 
-  const {
-    premiumAds,
-    nonPremiumAds,
-    contentAds,
-    eventData,
-    partnerPage,
-    frontPageData,
-  } = await initCommon();
-
-  // init job ad component in front feed
-  const desktopRowNode = initDesktopRowLoading();
-  if (document.getElementById('desktop-rows'))
-    document.getElementById('desktop-rows').append(desktopRowNode);
-  const JobAdsComponentNode = initJobAdsComponentLoading(10);
-  const eventCardsListNode = initEventCardsListLoading();
-  // init box that shows job listings
-  console.log(frontPageData);
-  initDesktopRow(desktopRowNode, frontPageData.frontpage);
-
-  initJobAdsComponent(
-    premiumAds,
-    JobAdsComponentNode,
-    'replace',
-    'utvalgte stillinger'
-  );
+  const { premiumAds, contentAds, partnerPage } = await initCommon();
 
   // if we have a partner page, we should not show ads
   if (!partnerPage) {
@@ -68,7 +29,7 @@ export async function initFrontend() {
     // init component that shows content articles and job listings in front feed
     const frontAdElements = initFrontAdComponents(
       getNodes(
-        '.article-previews .row:not(.show-for-small-only, .show-for-medium-up, .added)',
+        ".article-previews .row:not(.show-for-small-only, .show-for-medium-up, .added)",
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
       ),
       [
