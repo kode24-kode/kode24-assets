9d16dddcd2c62c28a9954e03de762563f446e964
Jørgen Jacobsen <jorgeja@gmail.com>
2023-02-02 13:15:50 +0100

    added alt tag to partner image and changed references to h1 with .headline

--- scripts/components/diamondPartners.js
+++ scripts/components/diamondPartners.js
@@ -4,8 +4,8 @@
 
 export function initDiamondPartners(diamondPartners, selector) {
   if (diamondPartners && diamondPartners.length) {
-    var diamondPartnersContainer = document.createElement("div");
-    diamondPartnersContainer.classList.add("diamond-partners-list");
+    var diamondPartnersContainer = document.createElement('div');
+    diamondPartnersContainer.classList.add('diamond-partners-list');
     diamondPartnersContainer.innerHTML = `
     <h3>Se karrieremuligheter hos våre partnere</h3>
     <ul>
@@ -13,14 +13,15 @@ export function initDiamondPartners(diamondPartners, selector) {
       .map(
         (partner) => `
       <li>
-      <a href="${partner.url}"><img src="${partner.logo}"></a>
+      <a href="${partner.url}"><img src="${partner.logo}" alt="partner logo ${partner.name}"></a>
       </li>
       `
       )
-      .join("")}
+      .join('')}
       </ul>
       `;
+    document
+      .querySelector(selector)
+      .prepend(diamondPartnersContainer);
   }
-
-  document.querySelector(selector).prepend(diamondPartnersContainer);
 }
