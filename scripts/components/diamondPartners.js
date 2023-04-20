09b1dc5b3ec19acd2d2dbe6c4179f5cbf2ef8871
Jørgen Jacobsen <jorgeja@gmail.com>
2023-04-20 21:57:01 +0200

    diamond partner pwc

--- scripts/components/diamondPartners.js
+++ scripts/components/diamondPartners.js
@@ -1,4 +1,4 @@
-import { shuffleArray } from "../functions/shuffleArray.js";
+import { shuffleArray } from '../functions/shuffleArray.js';
 /**
  * Displays box of diamond partners
  */
@@ -12,146 +12,160 @@ export function initDiamondPartners(node) {
         (partner) => `
       <li>
       <a href="${partner.url}">
-        <img class="dark" src="${partner.darkLogo}" alt="partner logo ${
-          partner.name
-        }">
-        <img class="light" src="${partner.lightLogo}" alt="partner logo ${
-          partner.name
-        }">
-        ${partner.title && `<span class="description">${partner.title}</span>`}
+        <img class="dark" src="${
+          partner.darkLogo
+        }" alt="partner logo ${partner.name}">
+        <img class="light" src="${
+          partner.lightLogo
+        }" alt="partner logo ${partner.name}">
+        ${
+          partner.title &&
+          `<span class="description">${partner.title}</span>`
+        }
       </a>
       </li>
       `
       )
-      .join("")}
+      .join('')}
       </ul>
       `;
 }
 
 export function initDiamondPartnersLoading() {
-  var diamondPartnersContainer = document.createElement("div");
-  diamondPartnersContainer.classList.add("diamond-partners-list");
+  var diamondPartnersContainer = document.createElement('div');
+  diamondPartnersContainer.classList.add('diamond-partners-list');
   return diamondPartnersContainer;
 }
 
 const partners = [
   {
-    name: "DNB",
+    name: 'DNB',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-dnb-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-dnb-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-dnb-dark.png",
-    title: "We Aim Higher",
-    url: "https://www.kode24.no/partner/dnb",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-dnb-dark.png',
+    title: 'We Aim Higher',
+    url: 'https://www.kode24.no/partner/dnb',
   },
   {
-    name: "Avantgarde Search",
+    name: 'Avantgarde Search',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-avantgardesearch-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-avantgardesearch-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-avantgardesearch-dark.png",
-    title: "Looking for talent?",
-    url: "https://www.kode24.no/partner/avantgarde-search",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-avantgardesearch-dark.png',
+    title: 'Looking for talent?',
+    url: 'https://www.kode24.no/partner/avantgarde-search',
   },
   {
-    name: "Enso",
+    name: 'Enso',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-enso-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-enso-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-enso-dark.png",
-    title: "Mennesker du vil jobbe med",
-    url: "https://www.kode24.no/partner/enso",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-enso-dark.png',
+    title: 'Mennesker du vil jobbe med',
+    url: 'https://www.kode24.no/partner/enso',
   },
   {
-    name: "Telenor",
+    name: 'Telenor',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-telenor-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-telenor-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-telenor-dark.png",
-    logoUrl: "telenor",
-    title: "Vi har mot til å forandre – har du?",
-    url: "https://www.kode24.no/partner/telenor",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-telenor-dark.png',
+    logoUrl: 'telenor',
+    title: 'Vi har mot til å forandre – har du?',
+    url: 'https://www.kode24.no/partner/telenor',
   },
   {
-    name: "Bouvet",
+    name: 'Bouvet',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-bouvet-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-bouvet-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-bouvet-dark.png",
-    title: "Vi er samfunsbyggere",
-    url: "https://www.kode24.no/partner/bouvet",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-bouvet-dark.png',
+    title: 'Vi er samfunsbyggere',
+    url: 'https://www.kode24.no/partner/bouvet',
   },
   {
-    name: "Shortcut",
+    name: 'Shortcut',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-shortcut-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-shortcut-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-shortcut-dark.png",
-    title: "We make apps for life",
-    url: "https://www.kode24.no/partner/shortcut",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-shortcut-dark.png',
+    title: 'We make apps for life',
+    url: 'https://www.kode24.no/partner/shortcut',
   },
   {
-    name: "Geodata",
+    name: 'Geodata',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-geodata-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-geodata-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-geodata-dark.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-geodata-dark.png',
 
-    title: "Vit hvor du er – vit hvor du skal",
-    url: "https://www.kode24.no/partner/geodata",
+    title: 'Vit hvor du er – vit hvor du skal',
+    url: 'https://www.kode24.no/partner/geodata',
   },
   {
-    name: "Computas",
+    name: 'Computas',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-computas-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-computas-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-computas-dark.png",
-    title: "Mulig du er et geni, men funker du sammen med andre glupinger?",
-    url: "https://www.kode24.no/partner/computas",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-computas-dark.png',
+    title:
+      'Mulig du er et geni, men funker du sammen med andre glupinger?',
+    url: 'https://www.kode24.no/partner/computas',
   },
   {
-    name: "Twoday",
+    name: 'Twoday',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-twoday-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-twoday-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-twoday-dark.png",
-    title: "La oss kode fremtiden sammen",
-    url: "https://www.kode24.no/partner/twoday",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-twoday-dark.png',
+    title: 'La oss kode fremtiden sammen',
+    url: 'https://www.kode24.no/partner/twoday',
   },
   {
-    name: "Boitano",
+    name: 'Boitano',
     lightLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-boitano-light.png",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-boitano-light.png',
     darkLogo:
-      "https://www.kode24.no/files/2023/02/17/kode24-partner-boitano-dark.png",
-    title: "Jobb med alt fra start-ups til de største merkevarene.",
-    url: "https://www.kode24.no/partner/boitano",
+      'https://www.kode24.no/files/2023/02/17/kode24-partner-boitano-dark.png',
+    title: 'Jobb med alt fra start-ups til de største merkevarene.',
+    url: 'https://www.kode24.no/partner/boitano',
   },
   {
-    name: "Skatteetaten",
+    name: 'Skatteetaten',
     lightLogo:
-      "https://www.dagbladet.no/files/2023/03/03/kode24-partner-skatteetaten2-light.png",
+      'https://www.dagbladet.no/files/2023/03/03/kode24-partner-skatteetaten2-light.png',
     darkLogo:
-      "https://www.dagbladet.no/files/2023/03/03/kode24-partner-skatteetaten2-dark.png",
-    title: "Bli med på å gjøre Norge enklere å bruke",
-    url: "https://www.kode24.no/partner/skatteetaten",
+      'https://www.dagbladet.no/files/2023/03/03/kode24-partner-skatteetaten2-dark.png',
+    title: 'Bli med på å gjøre Norge enklere å bruke',
+    url: 'https://www.kode24.no/partner/skatteetaten',
   },
   {
-    name: "Noa Ignite",
+    name: 'Noa Ignite',
     lightLogo:
-      "https://www.dagbladet.no/files/2023/03/07/kode24-partner-noa-ignite-light.png",
+      'https://www.dagbladet.no/files/2023/03/07/kode24-partner-noa-ignite-light.png',
     darkLogo:
-      "https://www.dagbladet.no/files/2023/03/07/kode24-partner-noa-ignite-dark.png",
-    title: "Vi er Norges ledende hus på Sanity og Optimizely",
-    url: "https://www.kode24.no/partner/noa-ignite",
+      'https://www.dagbladet.no/files/2023/03/07/kode24-partner-noa-ignite-dark.png',
+    title: 'Vi er Norges ledende hus på Sanity og Optimizely',
+    url: 'https://www.kode24.no/partner/noa-ignite',
   },
   {
-    name: "Kodebyraaet",
+    name: 'Kodebyraaet',
     lightLogo:
-      "https://www.dagbladet.no/files/2023/03/10/kode24-partner-kodebyraaet-light.png",
+      'https://www.dagbladet.no/files/2023/03/10/kode24-partner-kodebyraaet-light.png',
     darkLogo:
-      "https://www.dagbladet.no/files/2023/03/10/kode24-partner-kodebyraaet-dark.png",
-    title: "At Kodebyraaet we always strive to make things with an edge, ",
-    url: "https://www.kode24.no/partner/kodebyraaet",
+      'https://www.dagbladet.no/files/2023/03/10/kode24-partner-kodebyraaet-dark.png',
+    title:
+      'At Kodebyraaet we always strive to make things with an edge, ',
+    url: 'https://www.kode24.no/partner/kodebyraaet',
+  },
+  {
+    name: 'PwC',
+    lightLogo:
+      'https://www.dagbladet.no/files/2023/04/20/kode24-partner-pwc-light.png',
+    darkLogo:
+      'https://www.dagbladet.no/files/2023/04/20/kode24-partner-pwc-dark.png',
+    title: 'Konsulent eller inhouse? Du velger!',
+    url: 'https://www.kode24.no/partner/pwc',
   },
 ];
