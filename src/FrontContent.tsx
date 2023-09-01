062b4d24a2a19f6aa724557d1787845483f722c1
Jørgen Jacobsen <jorgeja@gmail.com>
2023-09-01 12:19:56 +0200

    updated so no empty news

--- src/FrontContent.tsx
+++ src/FrontContent.tsx
@@ -220,8 +220,7 @@ export default function FrontContent(frontpageData: Frontpage) {
                     antall: 2,
                     lenke: '',
                     articles:
-                      frontPageDataCopy.content.length > 0 ||
-                      frontPageDataCopy.listing.listings.length > 0
+                      frontPageDataCopy.content.length > 0
                         ? frontpageData.latestArticles.splice(0, 1)
                         : frontpageData.latestArticles.splice(0, 2),
                   }}
