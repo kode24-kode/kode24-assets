aac3720c92376c0e862bda7c4f9c66310a7410b3
Jørgen Jacobsen <jorgeja@gmail.com>
2024-02-22 10:21:47 +0100

    updated podcastplayer

--- src/components/PodcastPlayer.tsx
+++ src/components/PodcastPlayer.tsx
@@ -1,12 +1,10 @@
 export default function PodcastPlayer() {
   return (
     <iframe
-      src="https://omny.fm/shows/kode24-timen/playlists/podcast/embed?style=artwork&image=1&share=1&download=1&description=1&follow=1&playlistimages=0&playlistshare=1&foreground=f8f8f2&background=23252f&highlight=e92b93"
-      allow="autoplay; clipboard-write"
-      width="100%"
-      height="500"
+      src="https://embed.acast.com/65d327d9fff35b0016807558?episode-order=desc&accentColor=dedee2&bgColor=232630&secondaryColor=f23197&font-family=IBM%20Plex%20Sans&font-src=https%3A%2F%2Ffonts.googleapis.com%2Fcss%3Ffamily%3DIBM%2BPlex%20Sans"
       frameBorder="0"
-      title="kode24-timen"
+      width="100%"
+      height="190px"
     ></iframe>
   );
 }
