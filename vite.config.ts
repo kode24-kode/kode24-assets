30f8c37e02b5c3583b73791c335038201dd2a698
Jørgen Jacobsen <jorgeja@gmail.com>
2024-11-13 12:27:37 +0100

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "kode24",
-    project: "javascript"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'kode24',
+      project: 'javascript',
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
@@ -22,6 +25,6 @@ export default defineConfig({
       },
     },
 
-    sourcemap: true
+    sourcemap: true,
   },
 });
