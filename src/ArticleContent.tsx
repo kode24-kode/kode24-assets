d8f94f33e9c795a65a8a2d9aec823c149cf63f4e
Jørgen Jacobsen <jorgeja@gmail.com>
2025-05-27 11:45:55 +0200

    updated

--- src/ArticleContent.tsx
+++ src/ArticleContent.tsx
@@ -13,50 +13,50 @@ import Banner from "./components/Banner.tsx";
 export default function FrontContent(frontpageData: Frontpage) {
   (window as any).hljs.highlightAll();
 
-  // So we don't mutate the original data
-  const frontPageDataCopy = structuredClone(frontpageData) as Frontpage;
-  /** shuffle content and ads */
-  frontPageDataCopy.content = shuffleArray(frontPageDataCopy.content) as [
-    Content
-  ];
-  // get only premium ads and shuffle them
-  frontPageDataCopy.listing.listings = shuffleArray(
-    frontPageDataCopy.listing.listings.filter((listing) =>
-      frontpageData.listing.premiumIds.includes(listing.id)
-    )
-  ) as [Listing];
-
-  // grab the DOM-elements for the three content divs
-
-  /** This part should only occur in articles */
-  /** Attempts to add job ads before every odd h2-tag in article */
-
-  const bannerAds = frontPageDataCopy.bannerAds.filter(
-    (ad) => ad.adFormat === "desktop-brandboard_980x600"
-  );
-
-  const mobileBannerAds = frontPageDataCopy.bannerAds.filter(
-    (ad) => ad.adFormat === "mobile-banner_320x250"
-  );
-
-  // create node for banners and add before #hyvor-talk-view
-  const bannerNode = document.createElement("div");
-  ReactDOM.createRoot(bannerNode as HTMLElement).render(
-    <React.StrictMode>
-      <>
-        {bannerAds.length > 0 && (
-          <Banner ads={bannerAds} mobileToggle={false} />
-        )}
-        {mobileBannerAds.length > 0 && (
-          <Banner ads={mobileBannerAds} mobileToggle={true} />
-        )}
-      </>
-    </React.StrictMode>
-  );
-
-  document?.getElementById("hyvor-talk-view")?.before(bannerNode);
-
   if (document.querySelector(".article-entity:not(.is-editor)")) {
+    // So we don't mutate the original data
+    const frontPageDataCopy = structuredClone(frontpageData) as Frontpage;
+    /** shuffle content and ads */
+    frontPageDataCopy.content = shuffleArray(frontPageDataCopy.content) as [
+      Content
+    ];
+    // get only premium ads and shuffle them
+    frontPageDataCopy.listing.listings = shuffleArray(
+      frontPageDataCopy.listing.listings.filter((listing) =>
+        frontpageData.listing.premiumIds.includes(listing.id)
+      )
+    ) as [Listing];
+
+    // grab the DOM-elements for the three content divs
+
+    /** This part should only occur in articles */
+    /** Attempts to add job ads before every odd h2-tag in article */
+
+    const bannerAds = frontPageDataCopy.bannerAds.filter(
+      (ad) => ad.adFormat === "desktop-brandboard_980x600"
+    );
+
+    const mobileBannerAds = frontPageDataCopy.bannerAds.filter(
+      (ad) => ad.adFormat === "mobile-banner_320x250"
+    );
+
+    // create node for banners and add before #hyvor-talk-view
+    const bannerNode = document.createElement("div");
+    ReactDOM.createRoot(bannerNode as HTMLElement).render(
+      <React.StrictMode>
+        <>
+          {bannerAds.length > 0 && (
+            <Banner ads={bannerAds} mobileToggle={false} />
+          )}
+          {mobileBannerAds.length > 0 && (
+            <Banner ads={mobileBannerAds} mobileToggle={true} />
+          )}
+        </>
+      </React.StrictMode>
+    );
+
+    document?.getElementById("hyvor-talk-view")?.before(bannerNode);
+    document?.querySelector(".meta")?.after(bannerNode);
     // draw a listing before each h2
     const h2s = document.querySelectorAll(".bodytext>h2");
     h2s.forEach((h2, key: number) => {
@@ -89,7 +89,7 @@ export default function FrontContent(frontpageData: Frontpage) {
             <ContentsRow
               Contents={
                 shuffleArray([
-                  ...frontPageDataCopy.jobAdsSanity.splice(0, 2),
+                  ...frontPageDataCopy.jobAdsSanity.splice(0, 3),
                 ]) as ContentTile[]
               }
               listView={false}
