45cddf0c4afb27cc56f2441c884c0b6110c8b91a
Jørgen Jacobsen <jorgeja@gmail.com>
2025-11-21 14:25:40 +0100

    updated

--- src/functions/handleHamburgerMenuClick.ts
+++ src/functions/handleHamburgerMenuClick.ts
@@ -16,11 +16,36 @@ export function handleHamburgerMenuClick() {
       });
   }
 
+  //cconsst isToggled = true;
+
   document
     ?.getElementById("top-menu-toggle")
     ?.addEventListener("click", (event) => {
+      event.stopPropagation();
+      console.log("got here");
       event.preventDefault();
       document?.getElementById("top-menu-toggle")?.classList.toggle("active");
       document?.getElementById("left-menu")?.classList.toggle("active");
     });
 }
+
+window.addEventListener("click", () => {
+  if (
+    document?.getElementById("top-menu-toggle")?.classList.contains("active")
+  ) {
+    console.log("yo");
+    document?.getElementById("top-menu-toggle")?.classList.toggle("active");
+    document?.getElementById("left-menu")?.classList.toggle("active");
+  }
+});
+
+window.addEventListener("keydown", (event) => {
+  if (
+    document?.getElementById("top-menu-toggle")?.classList.contains("active") &&
+    event.key.toLowerCase() === "escape"
+  ) {
+    console.log("yo");
+    document?.getElementById("top-menu-toggle")?.classList.toggle("active");
+    document?.getElementById("left-menu")?.classList.toggle("active");
+  }
+});
